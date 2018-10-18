af46771b5215a0722090e6f8889209115f1eb482
Edgar Reynaga <edgar.reynaga@loop.colum.edu>
2018-10-18 18:05:42 -0500

    Final Form Edit by Edgar Reynaga

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# FireBase
-FireBase Assignment
+# FireBase - Train Scheduler Assignment
+
+### **Overview**
+
+In this assignment, I created a complex/dynamic form where the user can input their desired Train Information and be displayed the next available train and their wait time is also calculated. Using JavaScript for the core of the site and a dynamic button with jQuery to manipulate the HTML. Topped off with some Bootstrap and CSS to make a stylish and effective webpage. Made possible with displaying the time with Moment.js
+ - - -
+### **Interacting with the Form**
+
+When the user arrives to the page, they are given 4 preliminary forms to fill out with the train name, their destination, First Train Time, and Frquency. Upon clicking submit, their informaiton will be gatherd and calculated to be displayed above the form in an organized by order table/list.
+
+![Trivie Quiz](assets/images/Train.png)
+
+
+This Page was created with:
+```
+HTML
+CSS
+JavaScript
+jQuery
+Moment.js
+Bootstrap
+```
+
+[Click Here to check your train times](https://ticola.github.io/FireBase/)
---
+++ assets/javascript/app.js
@@ -0,0 +1,67 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDNUiMtqpBDkwZDZjqM9So0StFqC37O2bo",
+    authDomain: "train-project-cfdef.firebaseapp.com",
+    databaseURL: "https://train-project-cfdef.firebaseio.com",
+    projectId: "train-project-cfdef",
+    storageBucket: "train-project-cfdef.appspot.com",
+    messagingSenderId: "1035779857288"
+};
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
+
+$("#add-user").on("click", function (event) {
+    event.preventDefault();
+
+    var trainName = $("#trainname-input").val().trim();
+    var trainDestination = $("#destination-input").val().trim();
+    var firstTrain = $("#first-train-input").val().trim();
+    var trainFreq = $("#train-freq-input").val().trim();
+
+    var newTrain = {
+        name: trainName,
+        destination: trainDestination,
+        first: firstTrain,
+        frequency: trainFreq
+
+    };
+
+    firebase.database().ref().push(newTrain);
+
+    // console.log(newTrain.name);
+    // console.log(newTrain.destination);
+    // console.log(newTrain.first);
+    // console.log(newTrain.frequency);
+
+});
+
+database.ref().on("child_added", function (childSnapshot, prevChildKey) {
+
+    console.log(childSnapshot.val());
+
+    var trainName = childSnapshot.val().name;
+    var trainDestination = childSnapshot.val().destination;
+    var firstTrain = childSnapshot.val().first;
+    var trainFreq = childSnapshot.val().frequency;
+
+    var firstTrainConverted = moment(firstTrain, "HH:mm").subtract(1, "years");
+
+    var currentTime = moment();
+
+    var difference = moment().diff(moment(firstTrainConverted), "minutes");
+    var differenceConverted = moment(difference).format("hh:mm");
+
+
+    var tRemainder = difference % trainFreq;
+
+    var minAway = trainFreq - tRemainder;
+
+    var nextTrain1 = moment().add(minAway, "minutes");
+    var nextTrainFormatted = moment(nextTrain1).format("hh:mm");
+
+
+    $("#space-table > tbody").append("<tr><td>" + trainName + "</td><td>" + trainDestination + "</td><td>" + trainFreq + "</td><td>" + nextTrainFormatted + "</td><td>" + minAway + "</td></tr>");
+
+});
